f9bd0c2f7d4edd85b4a42fd40f3128a890cbe783
Fery <emailferyy@gmail.com>
2024-09-05 00:20:06 +0700

    cleanup-code

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -7,7 +7,7 @@ const catchAsync = require('../../utils/catchAsync');
 
 const router = express.Router();
 
-router.route('/register').post(validate(authValidation.register), authController.register);
+router.route('/register').post(auth('manageUsers'), validate(authValidation.register), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
@@ -34,6 +34,8 @@ module.exports = router;
  *   post:
  *     summary: Register as user
  *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
  *     requestBody:
  *       required: true
  *       content:
